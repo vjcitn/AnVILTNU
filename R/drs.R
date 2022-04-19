3a8fd9f8c52e8b5962af895fa519fcf860b43c08
Vince Carey <stvjc@channing.harvard.edu>
2022-04-19 11:58:09 -0400

    Update drs.R

--- R/drs.R
+++ R/drs.R
@@ -3,6 +3,8 @@
 #' @return character(1) signed URL
 #' @note the default value for drs_uri is a CCDG CRAM file reference; the associated
 #' CRAI file has DRS URI `drs://dg.4503:dg.4503/1447260e-654b-4f9a-9161-c511cbdd0f95`
+#' @examples
+#' drs_access()
 #' @export
 drs_access = function(drs_uri='drs://dg.4503:dg.4503/17141a26-90e5-4160-9c20-89202b369431') {
   proc = basilisk::basiliskStart(bsklenv)
