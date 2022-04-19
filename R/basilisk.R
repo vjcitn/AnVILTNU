a8922ec3b41b0dea9d619583c7e5a88a57591b4b
Vince Carey <stvjc@channing.harvard.edu>
2022-04-19 11:55:43 -0400

    Update basilisk.R

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,5 @@
 bsklenv <- basilisk::BasiliskEnvironment(envname="bsklenv",
-    pkgname="BiocTNU",
+    pkgname="BiocTNU", packages="pandas==1.3.5",  # obligatory packages value
     pip=c("terra-notebook-utils==0.9.3"))
 
 
