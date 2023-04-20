ff22ce24d0d1a3eaaf61ffc268d6c43d6b9cbb0e
Martin Morgan <Martin.Morgan@RoswellPark.org>
2023-04-20 11:18:40 -0400

    use fully resolved basilisk::BasiliskEnvironment() in R/basilisk.R
    - may be run before basilisk is imported?

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,9 +1,9 @@
 #' @importFrom basilisk BasiliskEnvironment basiliskStart basiliskStop
 #'     basiliskRun
 
-bsklenv <- BasiliskEnvironment(
+bsklenv <- basilisk::BasiliskEnvironment(
     envname = "bsklenv",
     pkgname = "BiocTNU",
     packages = "pandas==1.3.5", # obligatory packages value
-    pip = "terra-notebook-utils==0.11.0"
+    pip = "terra-notebook-utils==0.12.0"
 )
